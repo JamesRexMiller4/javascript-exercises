e130d671d190d5727dbad44d2c44eb2ce82ff7c3
James Miller <james.rex.miller.4@gmail.com>
2019-12-24 09:43:41 -0700

    Add passing functionality to repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if (num === 0) {
+    return ''
+  } else if (num < 0) {
+    return 'ERROR'
+  }
+  
+  let arr = [string]
+  for (let i=1; i < num; i++) {
+    arr.push(string)
+  }
+  return arr.join('')
 }
 
 module.exports = repeatString
